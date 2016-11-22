9db5f124c56436ddf6d4e728262660b0817d3bca
gerardoVillicana <gerardo_gevies@hotmail.com>
2016-11-22 10:04:52 -0600

    Subiendo

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+([
+/* 0 */
+/***/ function(module, exports) {
+
+	     'use strict';
+
+
+
+	        document.getElementById('calcBtn').addEventListener('click', function () {
+	           var principal = document.getElementById("principal").value;
+	           var years = document.getElementById("years").value;
+	           var rate = document.getElementById("rate").value;
+             let {monthlyPayment, monthlyRate, amortization} = mortgage.calculateAmortization(principal, years, rate);
+	            document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+	            document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+	            amortization.forEach(function (month) {
+	               return console.log(month);
+	            });
+	        });
+
+/***/ }
+]);
